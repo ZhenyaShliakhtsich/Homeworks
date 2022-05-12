f2ff58b5f028098dec9735660038f09b8df62dc1
ZhenyaShliakhtsich <shl.jeka@gmail.com>
2022-05-12 10:03:53 +0300

    HW15 без чтения из файла

--- app/src/main/java/com/example/homeworks/MainActivity.kt
+++ app/src/main/java/com/example/homeworks/MainActivity.kt
@@ -5,21 +5,38 @@ import android.os.Bundle
 import android.widget.TextView
 import com.example.homeworks.HW14.Classes
 import com.example.homeworks.HW14.Fibonachi
+import com.example.homeworks.HW15.Task1.Wine
+import com.example.homeworks.HW15.Task2.FixedPayment
+import com.example.homeworks.HW15.Task2.HourlyPayment
+import com.example.homeworks.HW15.Task2.Worker
+import java.io.BufferedWriter
+import java.io.File
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val fibonachi = Fibonachi()
-        val fib = fibonachi.fibonachi()
-        val classes = Classes()
-        classes.generation()
-        val heirExample = Classes.util(classes.clas1.a,classes.clas1.b)
-        val x = classes.clas1.sumOfFields()
+        val wine = Wine("da", "Dsa", GregorianCalendar(1975, 11, 12), "asd")
+        val hourlyworker = HourlyPayment("Gleb", 1234, "cook", 6.3)
+        val fixedworker = FixedPayment("Lisa", 1253, "cashier", 1000.0)
+        val fixedworker2 = FixedPayment("Nstya", 1921, "barmen", 1000.0)
+        val list = mutableListOf(fixedworker, fixedworker2, hourlyworker)
+        list.sortByDescending { it.payment }
+        println(list.slice(0..5))
+        println(list.slice(list.size - 4 until list.size))
+        val writer =
+            File("src/main/java/com/example/homeworks/HW15/Task2/output.txt").bufferedWriter()
+        list.forEach {
+            writer.write(it.toString())
+            writer.newLine()
+        }
+        writer.close()
+
 
 
         setContentView(R.layout.activity_main)
         findViewById<TextView>(R.id.textView).apply {
-            text = x.toString()
+            text = list.toString()
         }
     }
 }
